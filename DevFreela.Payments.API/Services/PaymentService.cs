3e299cfd3ad9e2a9d206c04ce746e6e94b665dac
Thiago Meloto <thiagommeloto@gmail.com>
2024-12-09 23:12:15 -0300

    Criação do microserviço que finaliza o projeto.

---
+++ DevFreela.Payments.API/Services/PaymentService.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Payments.API.Models;
+
+namespace DevFreela.Payments.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public PaymentService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public Task<bool> Process(PaymentInfoInputModel paymentInfoInputModel)
+        {
+            //Implementação da lógica de pagamento com Gateway de Pagamento.
+
+            return Task.FromResult(true);
+        }
+    }
+}
